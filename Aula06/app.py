0bf4fd90a9e29bdc11cce3f3d7184e48ddf8bff6
JF <joao.f.oliveira14@aluno.senai.br>
2025-08-26 11:36:20 -0300

    Aula06/Atividade

--- Aula06/app.py
+++ Aula06/app.py
@@ -1,8 +1,11 @@
 import json 
 import os
-
+#Base do codigo
 usuarios = []
 novo_arquivo = ''
+#Variaveis 
+
+limpar = lambda: os.system('cls' if os.name == 'nt' else 'clear')
 
 while True :
     usuario = {}
@@ -10,6 +13,7 @@ while True :
     print('2 - Salvar arquivo JSON,')
     print('3 - Fazer leitura do JSON,')
     print('4 - Sair do sistema,')
+    #Comandos que a gente seleciona 
 
     opcao = input ('Informe a opção desejada: ')
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -21,3 +25,41 @@ while True :
             usuario['email'] = input('Digite o email: ').strip().lower()
 
             usuarios.append(usuario)
+            limpar()
+            print('Usuário cadastrado com sucesso !')
+            continue
+        case '2':
+            novo_arquivo = input('Informe o nome do arquivo: ').strip().lower()
+
+            if novo_arquivo:
+                with open(f'Aula06/{novo_arquivo}.json', 'r', encoding= 'utf-8') as f :
+                    dados_existentes = json.load(f)
+
+            dados_existentes.extend(usuarios)
+
+            with open(f'Aula06/{novo_arquivo}.json', 'w', encoding= 'utf-8') as f :
+                json.dump(dados_existentes, f, ensure_ascii=False, indent=4)
+            limpar()
+            print('Arquivo salvo com sucesso!')
+            continue
+
+        case '3':
+            if not novo_arquivo:
+                novo_arquivo = input('Digite o nome do arquivo: ').strip().lower()
+            with open(f'Aula06/{novo_arquivo}.json', 'r', encoding= 'utf-8') as f:
+                dados = json.load(f)
+            print(f'{'-'*20} USUARIOS {'-'*20}\n')
+            for usuario in dados:
+                for chave in usuario:
+                    print(f'{chave.capitalize()}: {usuario.get(chave)}')
+                print('-'*40)
+            continue
+        case '4':
+            print('Saindo do sistema! ')
+            break
+        case _:
+            print('Opção invalida.')
+            continue
+
+                                                       
+                                 
---
+++ Aula06/app1.py
@@ -0,0 +1,18 @@
+#Dicionario
+
+usuario = {
+    'nome':'Gomes',
+    'idade': 26
+}
+
+print(type(usuario))
+print(usuario)
+
+#print(f'Nome: {usuario['nome']}')
+#print(f'Idade: {usuario['idade']}')
+
+usuarios = ['gomes', 'nunes', 'cris']
+print(usuarios)
+print(f'Nome: {usuarios[:]}')
+
+print(f' Nome: {usuario.get('nome')}')
