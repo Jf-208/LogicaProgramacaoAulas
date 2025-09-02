44972fbff4372d675868dd68ce49e230e5ab2463
JF <joao.f.oliveira14@aluno.senai.br>
2025-09-02 08:58:43 -0300

    Aula-7/Atividade

---
+++ Aula07/func.py
@@ -0,0 +1,12 @@
+def banc_usuario():
+    print("Bem Vindo(a) ao Sistema do Banco")
+
+    banc_usuario()
+''''
+while True :
+    usuario = {}
+    print('1 - Cadastrar novo usuario,')
+    print('2 - Salvar arquivo JSON,')
+    print('3 - Fazer leitura do JSON,')
+    print('4 - Sair do sistema,')
+'''
