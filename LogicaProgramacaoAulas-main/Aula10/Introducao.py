34e49314c236cef154b6ad5ebf59d6c329d9cd1f
JF <joao.f.oliveira14@aluno.senai.br>
2025-09-23 11:49:42 -0300

    Aula10real

---
+++ LogicaProgramacaoAulas-main/Aula10/Introducao.py
@@ -0,0 +1,29 @@
+# Paradigma Imperativo, e como seguir a receita de bolo, você dá comandos diretos ao computador, dizendo passo a passo que ele deve fazer.
+# Paradigma Estruturado e a evolução do imperativo onde organizamos os códigos em blocos bem definidos a fim de evitar confusão.
+# Paradigma Funcional trata tudo com funções matemáticas, sem modificar valores.
+# Paradigma Lógico você define fatos e regras e o computador deduz a respostas automaticamente. (IA, chatbots, sistema de recomendação)
+# POO (Paradigma Orientada a Objeto) Organiza o código como se cada parte do problema fosse um objeto, assim como no mundo real. (ex: def carro, def acelerar, def freiar)
+# - Classe espaço onde vou descrever um objeto (Pascal case - Inicial Maíuscula) 
+# - Atributos são as caractéristicas do personagem 
+# - Métodos são as ações que o personagem pode realizar
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
